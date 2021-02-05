87bb4793d042121b39ddf9a4d41270e497415b7f
206624202 <k.schmidt2@gmail.com>
2021-02-05 15:03:20 -0500

    adds chart:

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@ Highcharts.setOptions({
 
 
 function drawHighcharts() {
-    Highcharts.chart("chart-container-PROJECT-NAME", { // add the proper ID here
+    Highcharts.chart("chart-container-january-stocks", { // add the proper ID here
         chart: {
             type: 'bar',
             styledMode: true,
@@ -18,9 +18,17 @@ function drawHighcharts() {
         title: {
             text: null
         },
-        data: {
-            googleSpreadsheetKey: '1YOKb5l2VM4aAB2r20N_1aT_1vEajYrP3U-U3A6lZbC0',
-        },
+        series: [{
+            name: 'Final 11 months',
+            data: [29.9,5.7,-34.5,35,17.1,15.5,2.5,15.4,16.4],
+            pointPadding: -.4,
+            pointPlacement: -.15
+        },{
+            name: 'January',
+            data: [-2.7,-2.5,-6.1,-8.6,-3.7,-3.6,-3.1,-5.1,-.2,-1.1],
+            pointPadding: .2,
+            pointPlacement: .2 
+        }],
         // for bar charts only
         plotOptions: {
             series: {
@@ -49,7 +57,8 @@ function drawHighcharts() {
             symbolRadius: 0,
             verticalAlign: 'top',
             x: 10,
-            itemMarginTop: -10,
+            y: -10,
+            reversed: true
             // enabled: false
         },
         xAxis: {
@@ -62,6 +71,7 @@ function drawHighcharts() {
                 //     week: '%b. %e',
                 // },
             },
+            categories: [2003,2005,2008,2009,2010,2014,2015,2016,2020,2021],
             tickLength: 5,
             // tickInterval: 24 * 3600 * 1000 * 7
         },
@@ -74,9 +84,9 @@ function drawHighcharts() {
             //     return Highcharts.numberFormat(this.value,0,'.',',');
             // },
             },
-            // min: ,
-            // max: ,
-            // tickAmount: ,
+            min: -40,
+            max: 40,
+            tickAmount: 9,
             // adds commas to thousands
         },
         credits: {
@@ -100,7 +110,7 @@ function drawHighcharts() {
                 },
                 legend: {
                     align: 'left',
-                    x: -8
+                    x: -8,
                 },
                 tooltip: {
                     enabled: false
